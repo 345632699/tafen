267aebc299d7c1f477cae54859ded9060861d29a
xu <>
2018-08-14 22:36:40 +0800

    根据openid 判断用户

--- app/Api/Controllers/AuthController.php
+++ app/Api/Controllers/AuthController.php
@@ -79,4 +79,34 @@ class AuthController extends BaseController
         // the token is valid and we have found the user via the sub claim
         return response()->json(compact('user'));
     }
+
+    /**
+     * @api {post} /getUnionId 获取Token
+     * @apiName Wechat 获取Token及用户Id
+     * @apiGroup Wechat
+     *
+     *
+     * @apiParam {string} code wx.login接口获取到的CODE值
+     * @apiParam {string} encryptedData wx.getUserInfo 获取
+     * @apiParam {string} iv wx.getUserInfo 获取
+     *
+     * @apiSuccess {Array} data 返回的数据结构体
+     * @apiSuccess {Number} client_id 用于做二级分销URL后传递的parent_id 需要本地全局缓存
+     * @apiSuccess {string} token 请求接口 在header头中传递 或者在url的teken= ？ 中传递
+     *
+     * * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *          "response": {
+     *               "data": [
+     *                token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjE1LCJpc3MiOiJodHRwczovL2RqLm1xcGhwLmNvbS9nZXRVbmlvbklkIiwiaWF0IjoxNTM0MjU2MTEzLCJleHAiOjE1MzY4NDgxMTMsIm5iZiI6MTUzNDI1NjExMywianRpIjoiS1pKM1kxOTRBbjRvc0NVcSJ9.K4z-FqXcZWTGkcf8MFSvWYiAv0gRmOsDAb6jk7XGOYA",
+     *                client_id: 15
+     * ],
+     *               "status": 1,
+     *               "msg": "success"
+     *          }
+     *     }
+     *
+     *
+     */
 }
