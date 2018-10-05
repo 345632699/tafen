538183baed529c607515bf75540003326bbeff9a
xu <>
2018-10-06 00:04:49 +0800

    api

---
+++ app/Api/Controllers/Lesson/LessonController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Api\Controllers\Lesson;
+
+use App\Api\Controllers\BaseController;
+
+
+class LessonController extends BaseController
+{
+    public function updateTreeNode()
+    {
+
+    }
+}
---
+++ app/Model/Lesson.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lesson extends Model
+{
+    protected $table = "lessons";
+
+    protected $fillable = ['id', 'name', 'start_time', 'is_free', 'url', 'banner_bg', 'author', 'type', 'description'];
+
+    protected $primaryKey = 'id';
+
+
+}
