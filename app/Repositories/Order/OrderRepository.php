2534fe4c3f291c493b2da089ef58b705f9400c94
xu <>
2018-11-08 17:16:41 +0800

    选择规格 价格不更改的问题

--- app/Repositories/Order/OrderRepository.php
+++ app/Repositories/Order/OrderRepository.php
@@ -56,9 +56,16 @@ class OrderRepository implements OrderRepositoryInterface
         $good = DB::table('goods')->where('uid',$order_line_data['good_id'])->first();
         $order_line_data['buyer_msg'] = $request->get('buyer_msg',"");
         $order_line_data['quantity'] = $request->get('quantity',1);
-        $order_line_data['original_price'] = $good->original_price;
-        $order_line_data['discount_price'] = $good->discount_price;
         $order_line_data['attr_good_mapping_id'] = $request->get('attr_good_mapping_id','');
+        $attr_good_mapping_id = $request->get('attr_good_mapping_id', '');
+        if ($attr_good_mapping_id) {
+            $attr_mapping = DB::table('attr_good_mapping')->where('id', $attr_good_mapping_id)->first();
+            $order_line_data['original_price'] = $attr_mapping->original_price;
+            $order_line_data['discount_price'] = $attr_mapping->discount_price;
+        } else {
+            $order_line_data['original_price'] = $good->original_price;
+            $order_line_data['discount_price'] = $good->discount_price;
+        }
         $agentRate = $this->client->getAgentRate($client_id);
         $order_line_data['agent_price'] = $good->original_price * $agentRate / 100;
         if ($good->is_coupon) {
