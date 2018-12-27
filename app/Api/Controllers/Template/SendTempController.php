e7784616e5e4131f7db585c95f0e6115c7ebf242
xugf <xugf@mqphp.com>
2018-12-27 22:06:49 +0800

    上架产品限制

--- app/Api/Controllers/Template/SendTempController.php
+++ app/Api/Controllers/Template/SendTempController.php
@@ -11,7 +11,8 @@ use Illuminate\Http\Request;
 
 class SendTempController extends BaseController
 {
-    public function newOrder($order_lines,$client_id,$parent_id){
+    public function newOrder($order_lines, $client_id, $parent_id)
+    {
         $official_app = app('wechat.official_account');
         $official_parent_oepnid = findOfficialOpenid($parent_id);
         $official_client_oepnid = findOfficialOpenid($client_id);
@@ -21,24 +22,24 @@ class SendTempController extends BaseController
         $order_price = ''; //产品数量
         $time = ''; //产品数量
         foreach ($order_lines as $order_line) {
-            $good_name .= Good::find($order_line->good_id)->first()->name.",";
-            $good_num .= Good::find($order_line->good_id)->first()->name."x".$order_line->quantity.",";
-            $order_price .= Good::find($order_line->good_id)->first()->name.":".($order_line->total_price / 100)."元,";
+            $good_name .= Good::find($order_line->good_id)->first()->name . ",";
+            $good_num .= Good::find($order_line->good_id)->first()->name . "x" . $order_line->quantity . ",";
+            $order_price .= Good::find($order_line->good_id)->first()->name . ":" . ($order_line->total_price / 100) . "元,";
             $time = $order_line->created_at;
         }
-        if ($official_parent_oepnid != null){
+        if ($official_parent_oepnid != null) {
             $sendData = [
                 'touser' => $official_parent_oepnid,
                 'template_id' => '8QBuwuBrqHVJ935lVo3dJ5egO31i_m1XItEww7BCGns',
                 'path' => 'index',
                 'data' => [
-                    'first' => '恭喜您的下线'.$client->nick_name.'下单成功。',
+                    'first' => '恭喜您的下线' . $client->nick_name . '下单成功。',
                     'keyword1' => $good_name,
                     'keyword2' => $good_num,
                     'keyword3' => $order_price,
                     'keyword4' => $time,
-                    'keyword5' =>"包邮",
-                    'remark' =>"更多优惠项目，详情咨询客服"
+                    'keyword5' => "包邮",
+                    'remark' => "更多优惠项目，详情咨询客服"
                 ],
             ];
             \Log::info('===发送模板信息===sendData', $sendData);
@@ -46,7 +47,20 @@ class SendTempController extends BaseController
             \Log::info('===发送模板信息===resData' . json_encode($res));
         }
         if ($official_client_oepnid != null) {
-            $sendData['data']['first'] = '恭喜'.$client->nick_name.'下单成功。';
+            $sendData = [
+                'touser' => $official_parent_oepnid,
+                'template_id' => '8QBuwuBrqHVJ935lVo3dJ5egO31i_m1XItEww7BCGns',
+                'path' => 'index',
+                'data' => [
+                    'first' => '恭喜' . $client->nick_name . '下单成功。',
+                    'keyword1' => $good_name,
+                    'keyword2' => $good_num,
+                    'keyword3' => $order_price,
+                    'keyword4' => $time,
+                    'keyword5' => "包邮",
+                    'remark' => "更多优惠项目，详情咨询客服"
+                ]
+            ];
             \Log::info('===发送模板信息===sendData', $sendData);
             $res = $official_app->template_message->send($sendData);
             \Log::info('===发送模板信息===resData' . json_encode($res));
